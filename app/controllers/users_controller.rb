78475593fcc1083f357c1670f0fd40ab32ff451c
Shizuma <s1511536@u.tsukuba.ac.jp>
2017-07-31 11:37:35 +0900

    finish ch08

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-
 class UsersController < ApplicationController
 
   def show
@@ -12,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
